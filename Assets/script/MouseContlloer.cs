a6e02e552de3ff281bc754cf997c43be4f02b53b
bookman0156 <donaku15@gmail.com>
2020-07-10 00:06:22 +0900

    scene遷移

---
+++ Assets/script/MouseContlloer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MouseContlloer : MonoBehaviour
+{
+    Vector3 position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        position.z = -1.0f;
+        this.transform.position = position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        position.z = -1.0f;
+        this.transform.position = position;
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        string Tag = collision.gameObject.tag;
+        if(Tag == "Save")
+            if (Input.GetMouseButton(0))
+            {
+                SceneManager.LoadSceneAsync("SaveScene");
+            }
+
+        if (Tag == "Load")
+            if (Input.GetMouseButton(0))
+            {
+                SceneManager.LoadSceneAsync("LoadScene");
+            }
+
+        if (Tag == "Config")
+            if (Input.GetMouseButton(0))
+            {
+                SceneManager.LoadSceneAsync("ConfigScene");
+            }
+
+        if (Tag == "Title")
+            if (Input.GetMouseButton(0))
+            {
+                SceneManager.LoadSceneAsync("TitleScene");
+            }
+    }
+}
